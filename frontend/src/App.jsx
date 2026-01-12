315e500da1aef63d46ef8e27b72c36243a3cbc20
Utkarsh Solanki <160130824+UtkarshSolanki07@users.noreply.github.com>
2026-01-12 15:14:53 +0530

    vvv

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
