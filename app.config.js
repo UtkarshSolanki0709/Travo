0146686ed8573a7c65605188ca4e5cd7e57aa458
Utkarsh Solanki <160130824+UtkarshSolanki07@users.noreply.github.com>
2026-08-14 22:37:41 +0530

    app config fixing

--- app.config.js
+++ app.config.js
@@ -1,12 +1,40 @@
-module.exports = ({ config }) => ({
-  ...config,
-  android: {
-    ...config.android,
-    config: {
-      ...config.android?.config,
-      googleMaps: {
-        apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+const { withAppBuildGradle } = require('@expo/config-plugins');
+
+const withDuplicateManifestFix = (config) => {
+  return withAppBuildGradle(config, (config) => {
+    if (config.modResults.language === 'groovy') {
+      const patch = `
+android {
+    packagingOptions {
+        pickFirst 'META-INF/versions/9/OSGI-INF/MANIFEST.MF'
+        pickFirst 'META-INF/OSGI-INF/MANIFEST.MF'
+        pickFirst 'META-INF/LICENSE*'
+        pickFirst 'META-INF/NOTICE*'
+        pickFirst 'META-INF/DEPENDENCIES*'
+        pickFirst 'META-INF/INDEX.LIST'
+    }
+}
+`;
+      if (!config.modResults.contents.includes('META-INF/versions/9/OSGI-INF/MANIFEST.MF')) {
+        config.modResults.contents += patch;
+      }
+    }
+    return config;
+  });
+};
+
+module.exports = ({ config }) => {
+  const customConfig = {
+    ...config,
+    android: {
+      ...config.android,
+      config: {
+        ...config.android?.config,
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+        },
       },
     },
-  },
-});
+  };
+  return withDuplicateManifestFix(customConfig);
+};
